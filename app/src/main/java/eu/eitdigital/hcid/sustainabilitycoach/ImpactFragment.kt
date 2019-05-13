4d2c1339aea4659aa00189f970dce5cfbb843961
Vincent <giardina.v@gmail.com>
2019-05-13 13:48:05 +0200

    Stats bg

--- app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/ImpactFragment.kt
+++ app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/ImpactFragment.kt
@@ -9,7 +9,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
 import com.google.android.material.tabs.TabLayout
+import java.util.*
+import android.R.attr.entries
+import android.graphics.Color
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.XAxis
+
+
+
+
+
+
 
 class ImpactFragment: Fragment() {
     private lateinit var demoCollectionPagerAdapter: DemoCollectionPagerAdapter
@@ -66,6 +81,44 @@ class ImpactStatisticsFragment: Fragment() {
 }
 
 class ImpactHistoryFragment: Fragment() {
+    private lateinit var chart: LineChart
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.impact_history, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        chart = view.findViewById(R.id.chart)
+
+
+        chart.setDrawGridBackground(false)
+        chart.setMaxHighlightDistance(300F)
+        chart.getDescription().setEnabled(false)
+        chart.getLegend().setEnabled(false)
+
+        val y = chart.axisLeft
+        y.setLabelCount(6, false)
+        y.textColor = Color.WHITE
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+        y.setDrawGridLines(false)
+        y.axisLineColor = Color.WHITE
+
+        val entries = ArrayList<Entry>()
+        val values:Array<Float> = arrayOf(0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F)
+        for (i in 0..14) {
+            entries.add(Entry((i).toFloat(), values[i]))
+        }
+        val dataSet = LineDataSet(entries, "Volume of water saved")
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER)
+        dataSet.setCubicIntensity(0.2f)
+        dataSet.setDrawFilled(true)
+        dataSet.setDrawCircles(false)
+        dataSet.setLineWidth(1.8f)
+        dataSet.setCircleRadius(4f)
+        dataSet.setCircleColor(Color.WHITE)
+        dataSet.setDrawValues(false)
+        val lineData = LineData(dataSet)
+        chart.setData(lineData)
+        chart.invalidate()
+    }
 }
