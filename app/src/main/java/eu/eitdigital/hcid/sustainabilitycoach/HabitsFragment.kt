907f7ea72cfacf77d7adc56f57b953172b405030
leirelitw <litwin.leire@gmail.com>
2019-05-14 20:54:14 +0200

    changes
    -button of explore results
    -habits fragment

--- app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/HabitsFragment.kt
+++ app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/HabitsFragment.kt
@@ -1,10 +1,13 @@
 package eu.eitdigital.hcid.sustainabilitycoach
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import eu.eitdigital.hcid.sustainabilitycoach.explore.ExploreActivity
+import kotlinx.android.synthetic.main.habits_fragment.*
 
 class HabitsFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
@@ -13,4 +16,14 @@ class HabitsFragment: Fragment() {
     companion object {
         fun newInstance(): HabitsFragment = HabitsFragment()
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_create_habit.setOnClickListener {
+            val intent = Intent(activity, ExploreActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
