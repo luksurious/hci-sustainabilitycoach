95a7021f1f80e9919a08d1b9d479d241c7b38695
LAPTOP-S5BK4FJV\place <carlosmayarodriguez@gmail.com>
2019-05-17 20:01:03 +0200

    Filling results completely finished

--- app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/FillResultsActivity.kt
+++ app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/FillResultsActivity.kt
@@ -1,27 +1,37 @@
 package eu.eitdigital.hcid.sustainabilitycoach
 
-import android.graphics.Color
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
 import eu.eitdigital.hcid.sustainabilitycoach.plan.PlanFragmentInteractionListener
 import kotlinx.android.synthetic.main.activity_fill_results.*
 import kotlinx.android.synthetic.main.plan_success_dialog_fragment.*
+import com.google.android.material.snackbar.Snackbar
+import eu.eitdigital.hcid.sustainabilitycoach.model.DummyDataModel
+import eu.eitdigital.hcid.sustainabilitycoach.model.PREF_NAME
+import kotlinx.android.synthetic.main.activity_fill_results.*
+import kotlinx.android.synthetic.main.activity_fill_results.appToolbar
+import kotlinx.android.synthetic.main.activity_fill_results.fragment_container
 
 
 class FillResultsActivity : AppCompatActivity() {
+    private lateinit var preferences: DummyDataModel
 
     private var listener: PlanFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fill_results)
-
+        preferences = DummyDataModel(getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE));
         setSupportActionBar(appToolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //openFragment(HomeFillResults.newInstance(""))
+        var flag=false;
+
         goButton.setOnClickListener{
             // Get the checked radio button id from radio group
             var id: Int = radio_group.checkedRadioButtonId
@@ -29,15 +39,55 @@ class FillResultsActivity : AppCompatActivity() {
                 // Get the instance of radio button using id
                 val radio: RadioButton = findViewById(id)
                 //Toast.makeText(applicationContext,"On button click : ${radio.text}", Toast.LENGTH_SHORT).show()
+            if (id!=-1){
+                val radio: RadioButton = this.findViewById(id)
+                if(radio.text.equals("NO")){
+                    flag=true;
+                }else{
+                    showUnsupportedActionMessage()
+                }
+               if (restaurantOpt.isChecked && !(groceriesOpt.isChecked) && !(desireOpt.isChecked) && !(otherOpt.isChecked) && flag==true){
+                   flag=true;
+               }else{
+                   showUnsupportedActionMessage()
+                   flag=false;
+               }
+            }
+            if (flag){
+                preferences.state = DummyDataModel.States.FAILED_ONCE
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                Toast.makeText(applicationContext,"Your results were saved!", Toast.LENGTH_SHORT).show()
+                listener?.finishTaskToDashboard()
+            }
 
             }
-            Toast.makeText(applicationContext,"Your results were saved!", Toast.LENGTH_SHORT).show()
-            listener?.finishTaskToDashboard()
+
         }
 
 
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.explore_plan_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
 
+                return true
+            }
+            R.id.explore_plan_cancel -> {
+                finish()
+//                val intent = Intent(this, MainActivity::class.java)
+//                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     fun radio_button_click(view: View){
         // Get the clicked radio button instance
         val radio: RadioButton = findViewById(radio_group.checkedRadioButtonId)
@@ -50,4 +100,9 @@ class FillResultsActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT).show()*/
 
     }
+    fun  showUnsupportedActionMessage() {
+        Snackbar.make(fragment_container, "This selection is currently not supported!", Snackbar.LENGTH_LONG)
+            .show()
+    }
+
 }
---
+++ app/src/main/java/eu/eitdigital/hcid/sustainabilitycoach/FillResultsSucceedActivity.kt
@@ -0,0 +1,63 @@
+package eu.eitdigital.hcid.sustainabilitycoach
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.RadioButton
+import com.google.android.material.snackbar.Snackbar
+import eu.eitdigital.hcid.sustainabilitycoach.model.DummyDataModel
+import eu.eitdigital.hcid.sustainabilitycoach.model.PREF_NAME
+import kotlinx.android.synthetic.main.activity_fill_results_succeed.*
+import kotlinx.android.synthetic.main.activity_fill_results_succeed.appToolbar
+import kotlinx.android.synthetic.main.activity_fill_results_succeed.radio_group
+import kotlinx.android.synthetic.main.activity_fill_results_succeed.fragment_container
+
+class FillResultsSucceedActivity : AppCompatActivity() {
+    private lateinit var preferences: DummyDataModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fill_results_succeed)
+        preferences = DummyDataModel(getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE));
+        setSupportActionBar(appToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        goButton2.setOnClickListener {  var id: Int = radio_group.checkedRadioButtonId
+            if (id!=-1){
+                val radio: RadioButton = this.findViewById(id)
+                if(radio.text.equals("YES")){
+                    preferences.state = DummyDataModel.States.SUCCEEDED_ONCE
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }else{
+                    showUnsupportedActionMessage()
+                }
+            }
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.explore_plan_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+
+                return true
+            }
+            R.id.explore_plan_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun  showUnsupportedActionMessage() {
+        Snackbar.make(fragment_container, "This selection is currently not supported!", Snackbar.LENGTH_LONG)
+            .show()
+    }
+}
